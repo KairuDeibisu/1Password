491001ef1814d45454a597c40ba16257cc20040d
Kyle Davis <kyledavis957@gmail.com>
2024-08-12 11:06:30 -0400

    Fix: copyLib, update 1password libs

--- copyLib.ps1
+++ copyLib.ps1
@@ -4,17 +4,22 @@ Push-Location
 Set-Location $PSScriptRoot
 
 sudo {
-	$ptPath = "C:\Program Files\PowerToys"
+    $ptPath = "C:\Program Files\PowerToys"
 
-	@(
-		'PowerToys.Common.UI.dll',
-		'PowerToys.ManagedCommon.dll',
-		'PowerToys.Settings.UI.Lib.dll',
-		'Wox.Infrastructure.dll',
-		'Wox.Plugin.dll'
-	) | ForEach-Object {
-		New-Item ./Lib/$_ -ItemType SymbolicLink -Value "$ptPath\$_"
-	}
+    @(
+        'PowerToys.Common.UI.dll',
+        'PowerToys.ManagedCommon.dll',
+        'PowerToys.Settings.UI.Lib.dll',
+        'Wox.Infrastructure.dll',
+        'Wox.Plugin.dll'
+    ) | ForEach-Object {
+        $linkPath = "./Lib/$_"
+        if (-Not (Test-Path $linkPath)) {
+            New-Item $linkPath -ItemType SymbolicLink -Value "$ptPath\$_"
+        } else {
+            Write-Host "Symbolic link $linkPath already exists. Skipping..."
+        }
+    }
 }
 
 Pop-Location
